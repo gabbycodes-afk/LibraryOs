128fe9ff022cc65961d758631c982b27e57d4e18
gabbycodes-afk <teniolakolawole899@gmail.com>
2026-01-21 18:59:36 +0100

    final

--- core/settings.py
+++ core/settings.py
@@ -134,5 +134,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# ADD THIS LINE:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
